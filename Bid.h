1d9dc7610937de98a7e37d972cbb74b32cb3caf4
Wania-Khalid <waniakhalid906@gmail.com>
2026-05-01 13:23:00 +0500

    Member 4: Bid abstract class - polymorphism, operator overloading, friend function, static variable

---
+++ Bid.h
@@ -0,0 +1,35 @@
+#ifndef BID_H
+#define BID_H
+
+#include <iostream>
+using namespace std;
+
+// ABSTRACT CLASS (base for all bids)
+class Bid {
+protected:
+    int bidId;
+    double amount;
+    int userId;
+
+public:
+    static int totalBids;
+
+    // Constructor
+    Bid(int id, double amt, int uid);
+
+    // POLYMORPHISM (pure virtual → makes class abstract)
+    virtual void display() const = 0;
+
+    // Getter
+    double getAmount() const;
+
+    // OPERATOR OVERLOADING (comparison rule)
+    bool operator>(const Bid &b) const;
+
+    // FRIEND FUNCTION
+    friend bool compareBids(const Bid &b1, const Bid &b2);
+
+    virtual ~Bid() {}
+};
+
+#endif
